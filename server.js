e41f88881e742d13db42991e59b99950024eb767
itspiyush78 <itspiyush78@gmail.com>
2024-03-07 23:05:04 +0530

    connection done throw routes

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const db_config = require("./configs/db.config")
 const user_model = require("./models/user.model")
 const bcrypt = require("bcryptjs")
 
+app.use(express.json())
+
 /**
  * Create an admin user at the starting of the application
  * if not already present
@@ -57,6 +59,14 @@ console.log("Admin created", user)
      console.log("Error while create admin", err)
 }
 }
+
+
+
+/**
+ * Stich the route to the server 
+ */
+
+require("./routes/auth.routes")(app)
 
 /**
  * start the server 
